ac7aea1ba5f245762cdccbae333a120a278e8597
ProxVix <88912189+AgusMaciasITSV@users.noreply.github.com>
2023-04-15 17:29:11 -0300

    Guía 3 - Ej4

---
+++ Ejercicios/3_Manejo_de_exceptiones_con_python/Ej4.py
@@ -0,0 +1,16 @@
+#Este problema ya habia sido resuelto en el ejercicio 2, no se sabia el contenido del enunciado numero 4
+while True:
+    try:
+        a = int(input("Ingrese su primer numero: "))
+        b = int(input("Ingrese su segundo numero: "))
+        print(f"La division de los valores ingresados es: {a/b}\n")
+        x = input("¿Desea seguir dividiendo? Ingrese si para continuar, o cualquier otra cosa para cancelar: ")
+        if x == "si":
+            pass
+        else:
+            break
+
+    except ValueError:
+        print("El numero ingresado debe ser expresado en numeros decimales")
+    except ZeroDivisionError:
+        print("No se puede dividir por 0")
